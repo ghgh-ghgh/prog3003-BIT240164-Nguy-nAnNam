bad9a9ff4d68dabd2b331cd01e39f7cbd888b966
ghgh-ghgh <ghghntn@gmail.com>
2026-03-03 17:38:22 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+Manager<Student> = new Manager<>;
+Manager.add(new Student ("Sv01", "Nguyễn Long An", "4.0"));
+Manager.add(new Stundent ("Sv02", "Phạm Linh Sơn", "3.5"));
+Manager.add(new Stundent ("Sv03", "Trân Ngọc Thiên", "3.0"));
+ for (Student s : manager.getAll()){
+    System.out.println(s);
+}
+    }
